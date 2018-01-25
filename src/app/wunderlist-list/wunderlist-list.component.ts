05585ad9dfeedc0eab9d257fde7fe219a76e4b88
shyshenok <shyshenoka@gmail.com>
2018-01-25 19:14:48 +0200

    Create http request to wunderlist API for list of lists

--- src/app/wunderlist-list/wunderlist-list.component.ts
+++ src/app/wunderlist-list/wunderlist-list.component.ts
@@ -1,6 +1,7 @@
 import {Component, Injectable, OnInit} from '@angular/core';
 import { HttpClient } from "@angular/common/http";
 import {SharedTokenService} from "../services/shared-token.service";
+import {WunderList} from "../models/wunderList";
 
 @Injectable()
 
@@ -13,7 +14,7 @@ export class WunderlistListComponent implements OnInit {
 
   clientId: string = "0cfaf22850320aa5eb2c";
   headerToken: string;
-  wunderList: Array<Wunderlist>;
+  wunderList: WunderList;
 
 
   constructor(private httpClient: HttpClient,
@@ -23,9 +24,10 @@ export class WunderlistListComponent implements OnInit {
 
     this.headerToken = this.sharedServiceToken.sharedServiceToken.access_token;
 
-    this.httpClient.get("https://a.wunderlist.com/api/v1/lists", {headers: {'X-Access-Token': this.headerToken, 'X-Client-ID': this.clientId}})
-      .subscribe(value => {
-      console.log(value);
+    this.httpClient.get<WunderList>("https://a.wunderlist.com/api/v1/lists", {headers: {'X-Access-Token': this.headerToken, 'X-Client-ID': this.clientId}})
+      .subscribe(data => {
+        this.wunderList = data;
+        console.log(this.wunderList);
     });
   }
 
