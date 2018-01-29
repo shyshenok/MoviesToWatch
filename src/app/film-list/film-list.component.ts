3d817485e86c079d8738efcbf360165b3f839a12
shyshenok <shyshenoka@gmail.com>
2018-01-29 21:07:27 +0200

    Create http request for list films.

--- src/app/film-list/film-list.component.ts
+++ src/app/film-list/film-list.component.ts
@@ -1,7 +1,11 @@
-import { Component, OnInit } from '@angular/core';
-// import {MovieObject} from "../movie.service";
-// import  imdb from "imdb-api/lib/imdb";
+import {Component, Injectable, OnInit} from '@angular/core';
+import {HttpClient, HttpParams} from "@angular/common/http";
+import {SharedTokenService} from "../services/shared-token.service";
+import {WunderlistTasks} from "../models/wunderlistTasks";
+import {ActivatedRoute} from "@angular/router";
 
+
+@Injectable()
 
 @Component({
   selector: 'app-film-list',
@@ -11,14 +15,40 @@ import { Component, OnInit } from '@angular/core';
 
 export class FilmListComponent implements OnInit {
 
-  apiKey:string = "5942dc95";
-  // movie: MovieObject;
+  apiKey:string;
+  headerToken: string;
+  clientId: string = "0cfaf22850320aa5eb2c";
 
-  constructor() { }
+  taskList: WunderlistTasks;
+
+
+  constructor(private httpClient: HttpClient,
+              private sharedServiceToken: SharedTokenService,
+              private route: ActivatedRoute) { }
 
   ngOnInit() {
-    const imdb = require('imdb-api');
-    imdb.get('The Toxic Avenger', {apiKey: this.apiKey}).then(console.log).catch(console.log);
+  //
+  //   // const imdb = require('imdb-api');
+  //   // imdb.get('The Toxic Avenger', {apiKey: this.apiKey}).then(console.log).catch(console.log);
+  //   //
+  //
+    this.route.queryParams.subscribe(qParams => {
+
+      this.headerToken = this.sharedServiceToken.sharedServiceToken.access_token;
+
+      console.log("this.headerToken " + this.headerToken);
+
+      let params = new HttpParams();
+      params = params.append('list_id', qParams["list_id"]);
+
+      this.httpClient.get<WunderlistTasks>("https://a.wunderlist.com/api/v1/tasks", {headers: {'X-Access-Token': this.headerToken, 'X-Client-ID': this.clientId}, params: params},)
+        .subscribe(data => {
+          console.log('jjkhjkjljll');
+          console.log(data);
+        })
+    });
+
+
   }
 
 }
--- src/app/wunderlist-list/wunderlist-list.component.ts
+++ src/app/wunderlist-list/wunderlist-list.component.ts
@@ -2,6 +2,7 @@ import {Component, Injectable, OnInit} from '@angular/core';
 import { HttpClient } from "@angular/common/http";
 import {SharedTokenService} from "../services/shared-token.service";
 import {WunderList} from "../models/wunderList";
+import {Router} from "@angular/router";
 
 @Injectable()
 
@@ -15,24 +16,26 @@ export class WunderlistListComponent implements OnInit {
   clientId: string = "0cfaf22850320aa5eb2c";
   headerToken: string;
   wunderList: WunderList;
+  listName: string;
 
 
   constructor(private httpClient: HttpClient,
-              private sharedServiceToken: SharedTokenService) { }
+              private sharedServiceToken: SharedTokenService,
+              private router: Router) { }
 
   ngOnInit() {
 
     this.headerToken = this.sharedServiceToken.sharedServiceToken.access_token;
+    console.log(this.headerToken);
+
 
     this.httpClient.get<WunderList>("https://a.wunderlist.com/api/v1/lists", {headers: {'X-Access-Token': this.headerToken, 'X-Client-ID': this.clientId}})
       .subscribe(data => {
         this.wunderList = data;
         console.log(this.wunderList);
     });
-  }
-
-  goToFilmList() {
 
   }
+
 
 }
