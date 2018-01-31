12d7e9082a64e3c09bfe3196c1c79301822e504e
shyshenok <shyshenoka@gmail.com>
2018-01-31 15:45:47 +0200

    Create exit button

---
+++ src/app/exit-button/exit-button.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+import {Router} from "@angular/router";
+
+@Component({
+  selector: 'app-exit-button',
+  templateUrl: './exit-button.component.html',
+  styleUrls: ['./exit-button.component.css']
+})
+export class ExitButtonComponent implements OnInit {
+
+  constructor(private router: Router) { }
+
+  ngOnInit() {
+  }
+
+  goToAuthorization() {
+    localStorage.clear()
+    this.router.navigate(['']);
+  }
+
+}
