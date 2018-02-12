86224178071f6c022a091b4c01f9342bf5ef58e3
shyshenok <shyshenoka@gmail.com>
2018-02-12 16:28:59 +0200

    #1 Add parts of components

---
+++ src/app/template-button/template-button.component.ts
@@ -0,0 +1,16 @@
+import {Component, OnInit} from '@angular/core';
+
+@Component({
+  selector: 'app-exit-button',
+  templateUrl: './template-button.component.html',
+  styleUrls: ['./template-button.component.scss']
+})
+export class TemplateButtonComponent implements OnInit {
+
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
